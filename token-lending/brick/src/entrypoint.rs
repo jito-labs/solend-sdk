86746738e3c8289db6c18d2a0f2f6b421c74a2f1
0xripleys <105607696+0xripleys@users.noreply.github.com>
2023-08-20 21:35:59 -0400

    fix clippy (#163)

--- token-lending/brick/src/entrypoint.rs
+++ token-lending/brick/src/entrypoint.rs
@@ -4,14 +4,14 @@
 
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg,
-    program_error::PrintProgramError, program_error::ProgramError, pubkey::Pubkey,
+    program_error::ProgramError, pubkey::Pubkey,
 };
 
 entrypoint!(process_instruction);
 fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
 ) -> ProgramResult {
     msg!("got me feeling bricked up");
 
